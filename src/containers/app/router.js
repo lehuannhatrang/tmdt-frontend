210fb17c3701453a9e315cc8ffeb9d12da7629b0
Le Huan <lehuannhatrang98@gmail.com>
2019-11-02 12:21:10 +0700

    update

--- src/containers/app/router.js
+++ src/containers/app/router.js
@@ -3,6 +3,7 @@ import {Redirect, Route, Switch} from "react-router-dom";
 import Login from "../login";
 import DashBoard from "../dashboard";
 import NotFound from "../errors/NotFound";
+import Shopping from "../shopping";
 import If from "../../components/control/If";
 
 class Router extends Component {
@@ -15,6 +16,7 @@ class Router extends Component {
                 <Switch>
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/" component={DashBoard}/>
+                    <Route exact path="/shopping" component={Shopping} />
                     <Route path="" component={NotFound} />
                 </Switch>
             </div>
