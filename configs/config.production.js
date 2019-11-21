16225bb72d4daa07f61b3977a1abaaa8b3b7a7c1
Le Huan <lehuannhatrang98@gmail.com>
2019-11-22 00:17:46 +0700

    up

--- configs/config.production.js
+++ configs/config.production.js
@@ -1,5 +1,5 @@
 export const PRODUCTION_CONFIG = {
     BACKEND_HOST: 'https://vitinhvui.herokuapp.com',
-    FRONT_END_HOST: 'https://vitinhvui.com',
+    FRONT_END_HOST: 'https://vi-tinh-vui.herokuapp.com',
     BACKEND_API_URL: 'https://vitinhvui.herokuapp.com/api',
 }
